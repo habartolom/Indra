ff8758d5114b20c1336bb20e3cb8e0e14b344bf0
Harold Bartolo <habartolom@gmail.com>
2021-12-14 00:30:54 -0500

    First test

--- Indra/Controllers/TechnicalTestController.cs
+++ Indra/Controllers/TechnicalTestController.cs
@@ -17,7 +17,31 @@ namespace Indra.Controllers
         [HttpPost]
         public IActionResult First( [FromBody] FirstRequest request)
         {
-            var response = new int[] { 1, 2 };
+            var aux = request.LstCasas;
+            var response = new int[aux.Length];
+
+            if (aux.Length > 0)
+            {
+                for (int j = 0; j < request.Dias; j++)
+                {
+                    for (int i = 0; i < aux.Length; i++)
+                    {
+                        int firstNeighbor = 0;
+                        int secondNeighbor = 0;
+
+                        if (i > 0 && aux.Length > 1)
+                            firstNeighbor = aux[i - 1];
+
+                        if (i < aux.Length -1 && aux.Length > 1)
+                            secondNeighbor = aux[i + 1];
+
+                        response[i] = (firstNeighbor + secondNeighbor) % 2;
+                    }
+
+                    response.CopyTo(aux, 0);
+                }
+            }
+
             return Ok(response);
         }
 
